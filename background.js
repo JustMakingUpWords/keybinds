3f7193a6fc6db9282bf10c75c757ac067899a81f
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-21 17:53:38 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,31 +1,28 @@
-console.log("background script is running");
+console.log('background script is running');
 
-chrome.windows.onCreated.addListener(function(window) {
-    chrome.tabs.query({ currentWindow: true }, function(tabs) {
-        console.log(tabs);
-        if (tabs[0].url == "chrome://newtab/")
-        {
-            chrome.tabs.update(tabs[0].id, { url: 'https://google.com' });
-        }
-    });
+chrome.windows.onCreated.addListener(function (window) {
+  chrome.tabs.query({ currentWindow: true }, function (tabs) {
+    console.log(tabs);
+    if (tabs[0].url == 'chrome://newtab/') {
+      chrome.tabs.update(tabs[0].id, { url: 'https://google.com' });
+    }
+  });
 });
 
-chrome.commands.onCommand.addListener((command) => {
-    console.log(`Command: ${command}`);
+chrome.commands.onCommand.addListener(command => {
+  console.log(`Command: ${command}`);
 });
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    console.log(message);
-    if (message.action === "openNewTab")
-    {
-        // Open a new tab
-        chrome.tabs.create({ url: message.url });
-    }
-    if (message.action === "balls")
-    {
-        console.log("I GOT MESSAGE FROM CONTENT SCRIPT YAY");
-    }
-    /*
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  console.log(message);
+  if (message.action === 'openNewTab') {
+    // Open a new tab
+    chrome.tabs.create({ url: message.url });
+  }
+  if (message.action === 'balls') {
+    console.log('I GOT MESSAGE FROM CONTENT SCRIPT YAY');
+  }
+  /*
     if (message.action === "getChecked")
     {
         const checkbox1 = document.getElementById('checkbox1');
