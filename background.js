366d9e36b785c543f029a74bdd59abecd390518e
JustMakingUpWords <duc.vo.hm@gmail.com>
2023-12-07 01:33:42 -0600

    Update background.js

--- background.js
+++ background.js
@@ -3,25 +3,25 @@
 console.log('background script is running');
 
 chrome.windows.onCreated.addListener(function (window) {
-  chrome.tabs.query({ currentWindow: true }, function (tabs) {
-    console.log(tabs);
-    if (tabs[0].url == 'chrome://newtab/') {
-      chrome.tabs.update(tabs[0].id, { url: 'https://google.com' });
-    }
-  });
+    chrome.tabs.query({ currentWindow: true }, function (tabs) {
+        console.log(tabs);
+        if (tabs[0].url == 'chrome://newtab/') {
+        chrome.tabs.update(tabs[0].id, { url: 'https://google.com' });
+        }
+    });
 });
 
 chrome.commands.onCommand.addListener(command => {
-  console.log(`Command: ${command}`);
+    console.log(`Command: ${command}`);
 });
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  console.log(message);
-  if (message.action === 'openNewTab') {
-    // Open a new tab
-    chrome.tabs.create({ url: message.url.startsWith('https://') ? message.url : `https://${message.url}` });
-  }
-  /*
+    console.log(message);
+    if (message.action === 'openNewTab') {
+        // Open a new tab
+        chrome.tabs.create({ url: message.url.startsWith('https://') ? message.url : `https://${message.url}` });
+    }
+    /*
     if (message.action === "getChecked")
     {
         const checkbox1 = document.getElementById('checkbox1');
